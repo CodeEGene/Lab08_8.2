34f4231436c0863fc9997dd3c5d52c13d96e2418
ericschmidutz <e-schmidutz@onu.edu>
2024-03-27 17:29:51 -0400

    Completed P8-2.

--- Source.cpp
+++ Source.cpp
@@ -6,8 +6,13 @@ using namespace std;
 int countWords(string str);
 
 int main() {
-	
-
+	string input;
+	while (true) {
+		cout << "Enter a string or Q to quit: ";
+		getline(cin, input);
+		if (input == "Q") break;
+		cout << "Word Count: " << countWords(input) << endl;
+	}
 	return 0;
 }
 
