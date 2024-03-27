12bb92bcbe16537feb877ca845754b2ed7145381
ericschmidutz <e-schmidutz@onu.edu>
2024-03-27 17:29:32 -0400

    Completed coutnWords function.

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,29 @@
 #include<iostream>
+#include <string>
+
+using namespace std;
+
+int countWords(string str);
 
 int main() {
 	
 
-
 	return 0;
+}
+
+int countWords(string str) {
+	int count = 0;
+	bool letterBeforeSpace = true;
+
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] != ' ' && letterBeforeSpace) {
+			letterBeforeSpace = false;
+			count++;
+		}
+		if (str[i] == ' ') {
+			letterBeforeSpace = true;
+		}
+	}
+
+	return count;
 }
